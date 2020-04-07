3fd0332dafce3f5333ce9eb73cbf731abdcef024
yevgenyl <yevgeny@consist.co.il>
2020-04-07 15:13:25 +0300

    adding factory-resolver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {
     AfterViewInit,
     ChangeDetectionStrategy, ChangeDetectorRef,
-    Component,
+    Component, ComponentFactory, ComponentFactoryResolver, OnInit,
     TemplateRef,
     ViewChild,
     ViewContainerRef
 } from '@angular/core';
+import {ChildComponent} from "./child.component";
 
 
 
@@ -16,20 +17,24 @@ import {
     template: `
         <h2>App work</h2>
         <ng-container #container></ng-container>
-        <yl-child></yl-child>
     `,
     styles: [
 
     ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
    @ViewChild('container',{read: ViewContainerRef}) container: ViewContainerRef
+    private childFactory: ComponentFactory<ChildComponent>;
 
-    constructor() {
+    constructor(private componentFactoryResolver: ComponentFactoryResolver) {
+    }
+    ngOnInit(): void {
+     this.childFactory =  this.componentFactoryResolver.resolveComponentFactory(ChildComponent);
     }
 
     ngAfterViewInit(): void {
+       this.container.createComponent(this.childFactory);
 
     }
 }
